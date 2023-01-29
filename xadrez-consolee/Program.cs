7faab44df4bf019376c3544fc55606dfdad97407
Carolyna Wabiszczewicz <carol.wabiszczewicz@gmail.com>
2023-01-28 23:32:06 -0300

    Implementação inicial tabuleiro

--- xadrez-consolee/Program.cs
+++ xadrez-consolee/Program.cs
@@ -8,15 +8,9 @@ namespace xadrez_consolee
         static void Main(string[] args)
         {
 
-            Posicao P;
-
-            P = new Posicao(3, 4);
-
-            Console.WriteLine("Posição: " + P);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
             Console.ReadLine();
-
-         
 
     }
     }
