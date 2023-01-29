ba6537c207a872287d3ad67f8cc6ae02d40c0227
Carolyna Wabiszczewicz <carol.wabiszczewicz@gmail.com>
2023-01-28 23:21:59 -0300

    Criado classe Posicao

--- xadrez-consolee/Program.cs
+++ xadrez-consolee/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using tabuleiro;
 
-namespace xadrez_console
+namespace xadrez_consolee
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-        }
+
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.ReadLine();
+
+         
+
+    }
     }
 }
