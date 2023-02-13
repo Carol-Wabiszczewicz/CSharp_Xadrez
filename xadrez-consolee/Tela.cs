c4472a1cdffacdc44bff3ea8116d5e981ec00ef0
Carolyna Wabiszczewicz <carol.wabiszczewicz@gmail.com>
2023-02-12 22:51:34 -0300

    Testes de movimentação

--- xadrez-consolee/Tela.cs
+++ xadrez-consolee/Tela.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace xadrez_consolee
 {
@@ -29,6 +30,13 @@ namespace xadrez_consolee
             Console.WriteLine("  A B C D E F G H");
         }
 
+        public static PosicaoXadrez lerPosicaoXadrez()
+        {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1] + "");
+            return new PosicaoXadrez(coluna, linha);
+        }
         public static void imprimirPeca(Peca peca)
         {
             if(peca.cor == Cor.Branca)
